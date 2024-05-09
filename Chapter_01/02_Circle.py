4cce36d1e810d45e3927ce95af9dbc192e9ecb43
Rachit Garg <rachitgarg900@gmail.com>
2024-05-09 14:58:38 +0200

    Added a circle example

---
+++ Chapter_01/02_Circle.py
@@ -0,0 +1,20 @@
+from time import sleep
+from random import seed
+from random import randint
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((500, 400))
+
+while(True):
+    # This time successive non filled circles with random colors with line width of 5 will be created from centre of window
+    # Each increasing in size of 2 times the value of i for 100 times
+    for i in range (100):
+        seed(i)
+        pygame.draw.circle(window, (randint(0, 255), randint(0, 255), randint(0, 255)), (250, 200), i*2, 10)
+        pygame.display.update()
+        sleep(0.03)
+        # clear the screen by drawing a black rectangle equal to display size
+        pygame.draw.rect(window, (0, 0, 0), (0, 0, 500, 400)) 
+        pygame.display.update()
+        sleep(0.03)
